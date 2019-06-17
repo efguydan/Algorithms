bcf84ce39d1c45a6e1827864f69eb31ea8dd6bef
Efe Ejemudaro <efeejemudaro@gmail.com>
2019-06-17 13:37:55 +0100

    Fix: Fix JumpGame Solution

--- src/JumpGameII.java
+++ src/JumpGameII.java
@@ -1,37 +1,41 @@
-import java.util.Arrays;
-
 /**
  * 
  * @author EfeDaniel
- * Link: https://leetcode.com/problems/jump-game-ii/
- * Progress: In Progress
+ * Link: https://leetcode.com/problems/ju mp-game-ii/
+ * Progress: Done
  *
  */
 
 public class JumpGameII {
 	public static void main(String [] args) {
-		int [] nums1 = {1, 3, 4};
-		System.out.println(jump(nums1) + " " + (jump(nums1) == 2));
-		int [] nums2 = {1, 2, 3, 4, 5};
-		System.out.println(jump(nums2) + " " + (jump(nums2) == 3));
-		int [] nums3 = {2, 1};
-		System.out.println(jump(nums3) + " " + (jump(nums3) == 1));
-		int [] nums4 = {2, 1, 1};
-		System.out.println(jump(nums4) + " " + (jump(nums4) == 1));
-		int [] nums5 = {2, 1, 1, 1};
-		System.out.println(jump(nums5) + " " + (jump(nums5) == 2));
-		int [] nums6 = {2, 3, 1, 1, 1};
-		System.out.println(jump(nums6) + " " + (jump(nums6) == 2));
-        int [] nums7 = {4, 1, 1, 3, 1, 1, 1};
-        System.out.println(jump(nums7) + " " + (jump(nums7) == 2));
+		int answer = jump(new int[]{1, 3, 4});
+		System.out.println(answer + " " + (answer == 2));
+		answer = jump(new int[]{1, 2, 3, 4, 5});
+		System.out.println(answer + " " + (answer == 3));
+		answer = jump(new int[]{2, 1});
+		System.out.println(answer + " " + (answer == 1));
+		answer = jump(new int[]{2, 1, 1});
+		System.out.println(answer + " " + (answer == 1));
+		answer = jump(new int[]{2, 1, 1, 1});
+		System.out.println(answer + " " + (answer == 2));
+		answer = jump(new int[]{2, 3, 1, 1, 1});
+		System.out.println(answer + " " + (answer == 2));
+        answer = jump(new int[]{4, 1, 1, 3, 1, 1, 1});
+        System.out.println(answer + " " + (answer == 2));
+        answer = jump(new int[]{3, 2, 1});
+        System.out.println(answer + " " + (answer == 1));
+        answer = jump(new int[]{5, 3, 1, 1, 1, 2, 1});
+        System.out.println(answer + " " + (answer == 2));
+        answer = jump(new int[]{2, 3, 1});
+        System.out.println(answer + " " + (answer == 1));
+        answer = jump(new int[]{3 ,3 ,2 ,5 ,4 ,3 ,9 ,5 ,5 ,7 ,9 ,4 ,9 ,1 ,0 ,3 ,6 ,8 ,2});
+        System.out.println(answer + " " + (answer == 4));
 	}
 	
 	public static int jump(int[] nums) {
 		
         boolean atTheEnd = false;
-        int index = 0;
-        int noOfJumps = 0;
-        int bestJump = 0;
+        int index = 0, noOfJumps = 0, bestJump;
         int length = nums.length - 1;
         
         if(length == 0 || length == -1)
@@ -41,22 +45,24 @@ public class JumpGameII {
             bestJump = optimalJump(nums, index);
             index += bestJump;
             noOfJumps++;
-            if (index == length)
+            if (index >= length)
                 atTheEnd = true;
         }
         return noOfJumps;
     }
-    
+
     public static int optimalJump(int[] nums, int index){
-        int max = 0;
-        for(int i = 0; i < nums[index]; i++){
-            if(index + i > nums.length - 1)
-                continue;
-            if(nums[index + i] >= max)
+        int max = 0, indexOfmax = 0;
+        for (int i = 1; i <= nums[index]; i++) {
+            if (index + i >= nums.length - 1) {
+                indexOfmax = i;
+                break;
+            }
+            if (nums[index + i] + i > indexOfmax + max) {
                 max = nums[index + i];
+                indexOfmax = i;
+            }
         }
-        while(index + max > nums.length - 1)
-            max--;
-        return max;
+        return indexOfmax;
     }
 }
