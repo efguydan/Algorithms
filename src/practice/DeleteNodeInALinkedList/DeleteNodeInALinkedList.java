f0c68fc39d4272bb224e9492c7b793a4c726ca5d
EfeDaniel <EfeEjemudaro@gmail.com>
2020-01-10 13:46:30 +0100

    Feature: Add Delete Node in a Linked List Solution

---
+++ src/practice/DeleteNodeInALinkedList/DeleteNodeInALinkedList.java
@@ -0,0 +1,10 @@
+package practice.DeleteNodeInALinkedList;
+
+
+public class DeleteNodeInALinkedList {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
---
+++ src/practice/DeleteNodeInALinkedList/ListNode.java
@@ -0,0 +1,7 @@
+package practice.DeleteNodeInALinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
