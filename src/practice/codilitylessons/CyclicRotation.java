adf0b9164e672627451041a8a763d99b22ee6da2
EfeDaniel <EfeEjemudaro@gmail.com>
2020-02-17 00:11:17 +0100

    Feature: Add Cyclic Rotation Solution

---
+++ src/practice/codilitylessons/CyclicRotation.java
@@ -0,0 +1,20 @@
+package practice.codilitylessons;
+
+/**
+ * @author EfeDaniel
+ * Link: https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+ * Progress: Done
+ */
+public class CyclicRotation {
+
+    public int[] solution(int[] a, int k) {
+        int length = a.length;
+        int[] answer = new int[length];
+        for (int i = 0; i < length; i++) {
+            answer[(i + k) % length] = a[i];
+        }
+        return answer;
+    }
+
+
+}
