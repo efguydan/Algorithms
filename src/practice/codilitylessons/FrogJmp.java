f66b8484a12d739ced62348d4908bdfb28468b80
EfeDaniel <EfeEjemudaro@gmail.com>
2020-02-17 20:38:51 +0100

    Feature: Add FrogJmp Solution

---
+++ src/practice/codilitylessons/FrogJmp.java
@@ -0,0 +1,17 @@
+package practice.codilitylessons;
+
+/**
+ * @author EfeDaniel
+ * Link: https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
+ * Progress: Done
+ */
+public class FrogJmp {
+
+    public int solution(int x, int y, int d) {
+        int diff = y - x;
+        int answer = diff / d;
+        if (diff % d > 0) answer++;
+        return answer;
+    }
+
+}
