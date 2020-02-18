4f5a850ed44df354530dae9590eea6d36fd1ba61
EfeDaniel <EfeEjemudaro@gmail.com>
2020-02-18 15:40:10 +0100

    Feature: Add Missing Integer Solution

---
+++ src/practice/codilitylessons/MissingInteger.java
@@ -0,0 +1,22 @@
+package practice.codilitylessons;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MissingInteger {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3, 6, 4, 1, 2};
+        System.out.println(solution(nums1));
+    }
+
+    public static int solution(int[] a) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0) set.add(a[i]);
+        }
+        for (int j = 1; j <= set.size(); j++) {
+            if (!set.contains(j)) return j;
+        }
+        return set.size() + 1;
+    }
+}
