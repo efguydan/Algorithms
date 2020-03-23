ece8a94d8252e26891e72348c5797bb3ce73ab49
EfeDaniel <EfeEjemudaro@gmail.com>
2020-03-23 18:54:09 +0100

    Feature: Add sock merchant solution

---
+++ src/practice/hackerranklessons/SockMerchant.java
@@ -0,0 +1,27 @@
+package practice.hackerranklessons;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author EfeDaniel
+ * Link: www.hackerrank.com/challenges/sock-merchant/problem
+ * Progress: Done
+ */
+public class SockMerchant {
+
+    static int sockMerchant(int n, int[] ar) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            int current = ar[i];
+            if (set.contains(current)) {
+                answer++;
+                set.remove(current);
+            } else {
+                set.add(current);
+            }
+        }
+        return answer;
+    }
+}
