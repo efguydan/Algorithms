0d2767a5880260333a0dcf0861ea336f7b00fac3
EfeDaniel <EfeEjemudaro@gmail.com>
2020-03-23 15:19:10 +0100

    Feature: Add Allocation Solution

--- src/practice/MiscClass.java
+++ src/practice/MiscClass.java
@@ -1,16 +1,25 @@
 package practice;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Scanner;
 
 class MiscClass {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numberOfTest = input.nextInt();
 
+        for (int k = 0; k < numberOfTest; k++) {
+            int numberOfHouses = input.nextInt();
+            int priceToSpend = input.nextInt();
+            System.out.println(numberOfHouses);
+            System.out.println(priceToSpend);
+
+            int[] pricesOfHouseNext = new int[numberOfHouses];
+            for (int l = 0; l < numberOfHouses; l++) {
+                pricesOfHouseNext[l] = input.nextInt();
+            }
+            System.out.println(Arrays.toString(pricesOfHouseNext));
+        }
     }
 
 }
---
+++ src/practice/RecursionDavisStaircase.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class RecursionDavisStaircase {
+
+    public static void main(String[] args) {
+
+    }
+
+}
