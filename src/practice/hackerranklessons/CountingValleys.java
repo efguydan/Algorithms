403f4fd72a3762ac47a27e1215d033ecdb329cb9
EfeDaniel <EfeEjemudaro@gmail.com>
2020-03-23 19:10:25 +0100

    Feature: Add Countinf Valleys Solution

---
+++ src/practice/hackerranklessons/CountingValleys.java
@@ -0,0 +1,36 @@
+package practice.hackerranklessons;
+
+/**
+ * @author EfeDaniel
+ * Link: www.hackerrank.com/challenges/counting-valleys/problem
+ * Progress: Done
+ */
+public class CountingValleys {
+
+    public static void main(String[] args) {
+        System.out.println(countingValleys(8, "UDDDUDUU"));
+    }
+
+    static int countingValleys(int n, String s) {
+        int answer = 0;
+        int currentPos = 0;
+        boolean canAdd = true;
+        char[] array = s.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            currentPos += getValToAdd(array[i]);
+            if (currentPos < 0 && canAdd) {
+                answer++;
+                canAdd = false;
+            }
+            if (currentPos >= 0) {
+                canAdd = true;
+            }
+        }
+        return answer;
+    }
+
+    static int getValToAdd(char c) {
+        if (c == 'U') return 1; else return -1;
+    }
+
+}
