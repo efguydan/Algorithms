efc7ccf209fe5d03bd63af1a22b4a7540aaba3d0
EfeDaniel <EfeEjemudaro@gmail.com>
2020-04-01 22:24:48 +0100

    Feature: Add incomplete solution of Repeated String

---
+++ src/practice/hackerranklessons/RepeatedString.java
@@ -0,0 +1,28 @@
+package practice.hackerranklessons;
+
+/**
+ * @author EfeDaniel
+ * Link: www.hackerrank.com/challenges/repeated-string/problem
+ * Progress: Done
+ */
+public class RepeatedString {
+
+    public static void main(String[] args) {
+        System.out.println(repeatedString("aba", 10));
+    }
+
+    static long repeatedString(String s, long n) {
+        long answer = 0;
+        long noA = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') noA++;
+        }
+        answer += noA * n / s.length();
+        int toReach = (int) n % s.length();
+        for (int i = 0; i < toReach; i++) {
+            if (s.charAt(i) == 'a') answer++;
+        }
+        return answer;
+    }
+
+}
