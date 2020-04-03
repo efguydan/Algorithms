5853222c8426c03e70f25d4b0793830f2bff224a
EfeDaniel <EfeEjemudaro@gmail.com>
2020-04-04 00:14:00 +0100

    Chore: clean up code

--- src/practice/hackerranklessons/RepeatedString.java
+++ src/practice/hackerranklessons/RepeatedString.java
@@ -3,7 +3,7 @@ package practice.hackerranklessons;
 /**
  * @author EfeDaniel
  * Link: www.hackerrank.com/challenges/repeated-string/problem
- * Progress: Done
+ * Progress: Not Done
  */
 public class RepeatedString {
 
