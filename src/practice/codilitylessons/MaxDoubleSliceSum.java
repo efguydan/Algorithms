af8dfe7a8f1e8243ec9aaac64288e6ba4c08ba0e
EfeDaniel <EfeEjemudaro@gmail.com>
2020-04-12 23:25:46 +0100

    Feature: Add max Double Slice Sum solution

--- src/practice/codilitylessons/MaxDoubleSliceSum.java
+++ src/practice/codilitylessons/MaxDoubleSliceSum.java
@@ -1,7 +1,10 @@
 package practice.codilitylessons;
 
-import java.util.Arrays;
-
+/**
+ * @author EfeDaniel
+ * Link: https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_double_slice_sum/
+ * Progress: Done
+ */
 public class MaxDoubleSliceSum {
 
     public static void main(String[] args) {
@@ -10,16 +13,27 @@ public class MaxDoubleSliceSum {
     }
 
     public static int solution(int[] a) {
-        int[] start = new int[a.length];
-        start = goldenMaxSlice(a);
-        System.out.println(Arrays.toString(start));
-        return 0;
+        int[] start = maxSliceFromStart(a);
+        int[] end = maxSliceFromEnd(a);
+        int max = 0;
+        for (int i = 1; i < a.length - 1; i++) max = Math.max(max, start[i-1] + end[i+1]);
+        return max;
     }
 
-    private static int[] goldenMaxSlice(int[] a) {
+    private static int[] maxSliceFromStart(int[] a) {
         int[] answer = new int[a.length];
         int runningMax = 0;
         for (int i = 1; i < a.length - 1; i++) {
+            runningMax = Math.max(0, runningMax + a[i]);
+            answer[i] = runningMax;
+        }
+        return answer;
+    }
+
+    private static int[] maxSliceFromEnd(int[] a) {
+        int[] answer = new int[a.length];
+        int runningMax = 0;
+        for (int i = a.length - 2; i > 0; i--) {
             runningMax = Math.max(0, runningMax + a[i]);
             answer[i] = runningMax;
         }
