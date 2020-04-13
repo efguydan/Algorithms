a6e52de52708a771b7cb6f5bda286df1376ba74b
EfeDaniel <EfeEjemudaro@gmail.com>
2020-04-13 23:53:48 +0100

    Feature: Add MaxProfit Solution

--- src/practice/MiscKt.kt
+++ src/practice/MiscKt.kt
@@ -1,5 +1,7 @@
 package practice
 
 fun main() {
-
+    val sample = "OOTimiBoladeEE"
+    val arr = sample.split("TimiBolade".reversed().toRegex()).joinToString(separator = "")
+    println(arr)
 }
