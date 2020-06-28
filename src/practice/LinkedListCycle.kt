028d045c6f0530eaf6fa2bdd14c5631ca3d37cd3
EfeDaniel <EfeEjemudaro@gmail.com>
2020-06-28 15:15:49 +0100

    Feature: Add LinkedListCycle Solution

---
+++ src/practice/LinkedListCycle.kt
@@ -0,0 +1,29 @@
+package practice
+
+/**
+ * @author EfeDaniel
+ * Link: leetcode.com/problems/linked-list-cycle/
+ * Progress: Done
+ */
+class LinkedListCycle {
+
+    companion object {
+        fun hasCycle(head: ListNode?): Boolean {
+            if (head == null) return false
+            var tortoise = head
+            var hare = head
+
+            do {
+                tortoise = tortoise?.next
+                hare = hare?.next?.next
+            } while (tortoise != hare && tortoise != null && hare != null)
+
+            return tortoise != null && hare != null
+        }
+    }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+}
