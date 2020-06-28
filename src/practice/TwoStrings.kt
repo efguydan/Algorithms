0ee7325fb6545adde0fdf0282eceb3343b146432
EfeDaniel <EfeEjemudaro@gmail.com>
2020-06-28 14:16:11 +0100

    Chore: Add algo url to Two Strings and Sherlock Valid String Problem

--- src/practice/TwoStrings.kt
+++ src/practice/TwoStrings.kt
@@ -1,5 +1,11 @@
 package practice
 
+/**
+ * @author EfeDaniel
+ * Link: https://www.hackerrank.com/challenges/two-strings/problem
+ * Progress: Done
+ */
+
 //fun twoStrings(s1: String, s2: String): String  = s2.toSet().let {
 //    for (char in s1.toSet()) if (it.contains(char)) return "YES"
 //    return "NO"
