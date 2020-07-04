fbb324b9c53ed5b98857e8c24563b8f7ce0a5255
EfeDaniel <EfeEjemudaro@gmail.com>
2020-07-04 11:49:19 +0100

    Feature: Add Is Unique Solution

---
+++ src/practice/CtCI/arraysandstrings/IsUnique.java
@@ -0,0 +1,25 @@
+package practice.CtCI.arraysandstrings;
+
+/**
+ *  @author EfeDaniel
+ * Link: Ctci Page 90
+ * Progress: Passed all thinkable testcases yet
+ */
+public class IsUnique {
+
+    public static void main(String[] args) {
+        System.out.println(isUnique("efe"));
+        System.out.println(isUnique("daniel"));
+        System.out.println(isUnique(""));
+    }
+
+    private static boolean isUnique(String input) {
+        boolean[] charFound = new boolean[128];
+        for (char c : input.toCharArray()) {
+            if (charFound[c]) return false;
+            charFound[c] = true;
+        }
+        return true;
+    }
+
+}
