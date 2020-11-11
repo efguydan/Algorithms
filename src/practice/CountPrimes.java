2c73ae7835dfddfe1ff78e8d77953b4dd8cf2610
EfeDaniel <EfeEjemudaro@gmail.com>
2020-11-11 23:40:00 +0100

    Feature: Add Count Primes Solution

---
+++ src/practice/CountPrimes.java
@@ -0,0 +1,33 @@
+package practice;
+
+/**
+ * @author EfeDaniel
+ * Link: https://leetcode.com/problems/count-primes
+ * Progress: Done
+ */
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+        int[] array = new int[n];
+        if (n < 3) return 0;
+        int answer = 0;
+        array[0] = 1;
+        array[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            if (array[i] != 0) continue;
+            int j = i * 2;
+            while (j < n) {
+                array[j] = 1;
+                j += i;
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (array[i] == 0) answer++;
+        }
+
+        return answer;
+    }
+
+}
