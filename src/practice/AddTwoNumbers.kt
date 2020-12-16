2cf85538f4f6e4b2739191854a9eb092d4ad6c08
EfeDaniel <EfeEjemudaro@gmail.com>
2020-12-16 13:23:42 +0100

    Annotate AddTwoNumbers with explanations

--- src/practice/AddTwoNumbers.kt
+++ src/practice/AddTwoNumbers.kt
@@ -3,31 +3,58 @@ package practice
 /**
  * @author EfeDaniel
  * Link: https://leetcode.com/problems/add-two-numbers/
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
  * Progress: Done
  */
 
-private var head: ListNode? = null
-private var current: ListNode? = null
+object AddTwoNumbers {
 
-fun addTwoNumbers(l1: ListNode?, l2: ListNode?, overflow: Int = 0): ListNode? {
-    if (l1 == null && l2 == null) {
-        if (overflow > 0) addNode(overflow)
+    /**
+     * ListNode Class Definition
+     */
+    class ListNode(var `val`: Int) { var next: ListNode? = null }
+
+    /** Recursive Solution that builds a new linked list for the answer, hence the O(n) space complexity.
+     *  It takes the two relative numbers, when available, from the problem linked lists, adds them,
+     *  takes away overflow, if any, and adds that to the solution linked list. It then calls the method recursively
+     *  with the next element in both linked lists.
+     *
+     *  It stops the recursion when both linked lists are at a null solution; albeit not before it takes care
+     *  of any remaining overflow available
+     *
+     */
+
+    private var head: ListNode? = null
+    private var current: ListNode? = null
+
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?, overflow: Int = 0): ListNode? {
+
+        // Check if both linked lists are empty
+        if (l1 == null && l2 == null) {
+            // Takes care of remaining overflow before returning head
+            if (overflow > 0) addNode(overflow)
+            return head
+        }
+
+        //Add the two current nodes values, also taking care of the overflow
+        val temp = (l1?.`val` ?: 0) + (l2?.`val` ?: 0) + overflow
+        addNode(if (temp > 9) temp - 10 else temp)
+        addTwoNumbers(l1?.next, l2?.next, if (temp > 9) 1 else 0)
         return head
     }
-    val temp = (l1?.`val` ?: 0) + (l2?.`val` ?: 0) + overflow
-    addNode(if (temp > 9) temp - 10 else temp)
-    addTwoNumbers(l1?.next, l2?.next, if (temp > 9) 1 else 0)
-    return head
-}
 
-fun addNode(value: Int) {
-    if (head == null) {
-        head = ListNode(value)
-        current = head
-    } else {
-        current?.next = ListNode(value)
-        current = current?.next
+    /**
+     * Appends a node to the linked list or starts a new linked list
+     */
+    fun addNode(value: Int) {
+        if (head == null) {
+            head = ListNode(value)
+            current = head
+        } else {
+            current?.next = ListNode(value)
+            current = current?.next
+        }
     }
-}
 
-class ListNode(var `val`: Int) { var next: ListNode? = null }
+}
