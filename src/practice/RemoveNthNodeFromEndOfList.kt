6083a149ed2d6c1e936c03e9b1bf5a4446090f35
EfeDaniel <EfeEjemudaro@gmail.com>
2020-12-16 13:43:18 +0100

    Explain Solution for RemoveNthNodeFromEndOfList

--- src/practice/RemoveNthNodeFromEndOfList.kt
+++ src/practice/RemoveNthNodeFromEndOfList.kt
@@ -3,31 +3,50 @@ package practice
 /**
  *  @author EfeDaniel
  * Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/problem
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
  * Progress: Done
  */
 object RemoveNthNodeFromEndOfList {
 
-    // ListNode Class Definition
+    /**
+     * ListNode Class Declaration
+     */
     class ListNode(var `val`: Int) {
         var next: ListNode? = null
     }
 
-    // Main Method. For tests
+    /**
+     * Main method Used mainly for testing purposes. Tests the input case:
+     * linkedList = [1, 2, 3]
+     * n = 3
+     *
+     * output = [2, 3]
+     */
     @JvmStatic
     fun main(args: Array<String>) {
-        val l1 = ListNode(1).also { one ->
+        val linkedList = ListNode(1).also { one ->
             one.next = ListNode(2).also { two ->
                 two.next = ListNode(3)
             }
         }
 
-        var answer = removeNthFromEnd(l1, 3)
+        var answer = removeNthFromEnd(linkedList, 3)
         while (answer != null) {
             print("${answer.`val`} -> ")
             answer = answer.next
         }
     }
 
+    /**
+     * Solution Method
+     * It loops over the linked list, whilst keeping two pointers. One at the current index of the loop.
+     * The other n indexes behind the first index.
+     * At the end of the loop, it removes the next node to the second pointer.
+     *
+     * The same constant variables are created, hence an O(1) space complexity.
+     * The linked list is also looped over once, hence a linear time complexity.
+     */
     fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
         var h = head ?: return head
         val firstPointer = NodeIndex(0, h)
@@ -52,6 +71,10 @@ object RemoveNthNodeFromEndOfList {
         }
     }
 
+    /**
+     * NodeIndex Class Declaration.
+     * To keep note of a node and its position in the linked list.
+     */
     data class NodeIndex(
         var index: Int,
         var node: ListNode
