35f54920e0c726a7924126b7dbe7d8ce78877f3b
EfeDaniel <EfeEjemudaro@gmail.com>
2021-02-01 20:04:55 +0100

    Document InsertionSortPart2 Solution

--- src/practice/InsertionSortPart2.kt
+++ src/practice/InsertionSortPart2.kt
@@ -7,11 +7,22 @@ package practice
  */
 object InsertionSortPart2 {
 
+    /**
+     * Main method Used mainly for testing purposes. Tests the input case:
+     * n = 7
+     * arr = [3, 4, 7, 5, 6, 2, 1]
+     */
     @JvmStatic
     fun main(args: Array<String>) {
         insertionSort2(7, arrayOf(3, 4, 7, 5, 6, 2, 1))
     }
 
+    /**
+     * Solution Method
+     * Takes each value in the array and tracks back the previous indexes to find
+     * the correct position in the array.
+     * At the end of each iteration, it prints the current state of the array.
+     */
     fun insertionSort2(n: Int, arr: Array<Int>) {
         for (i in 1..arr.lastIndex) {
             var j = i
