1efab085551c587fd72a6c573e5d474d3c2ee783
EfeDaniel <EfeEjemudaro@gmail.com>
2021-02-02 13:07:03 +0100

    Document ValidAnagram Solution

--- src/practice/ValidAnagram.kt
+++ src/practice/ValidAnagram.kt
@@ -7,17 +7,31 @@ package practice
  */
 object ValidAnagram {
 
+    /**
+     * Main method Used mainly for testing purposes. Tests the input case:
+     * s = "anagram", t = "nagaram", output = true
+     * s= "a", t = "b", output = false
+     */
     @JvmStatic
     fun main(args: Array<String>) {
         println(isAnagram("anagram", "nagaram"))
         println(isAnagram("a", "b"))
     }
 
+    /**
+     * Solution Method. Sorts both strings and compares them with each other. If they are the same, they are anagrams
+     * and true is returned, else, they arent and false is returned.
+     *
+     * Bubble Sort isn't used for sorting because it times out for a input case
+     */
     fun isAnagram(s: String, t: String): Boolean {
         if (s.length != t.length) return false
         return s.sort() == t.sort()
     }
 
+    /**
+     * Method helper for sorting a string using bubble sort algorithm
+     */
     fun bubbleSort(string: String): String {
         if (string.length < 2) return string
         var arr = string.toCharArray()
@@ -34,6 +48,9 @@ object ValidAnagram {
         return arr.joinToString(separator = "")
     }
 
+    /**
+     * String sort helper method using the inbuilt algorithm
+     */
     private fun String.sort() = String(toCharArray().apply { sort() })
 
 }
