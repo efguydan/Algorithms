d053dce5c2321a2d7d88c7d6ad21fd2c1de99d58
EfeDaniel <EfeEjemudaro@gmail.com>
2021-02-03 12:56:19 +0100

    Chore: clean up code

--- src/practice/interviewcake/hashingandhashtables/PermutationPalindrome.java
+++ src/practice/interviewcake/hashingandhashtables/PermutationPalindrome.java
@@ -3,6 +3,11 @@ package practice.interviewcake.hashingandhashtables;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * @author EfeDaniel
+ * Link: https://www.interviewcake.com/question/java/permutation-palindrome?course=fc1&section=hashing-and-hash-tables
+ * Progress: Done
+ */
 public class PermutationPalindrome {
 
     public static boolean hasPalindromePermutation(String theString) {
