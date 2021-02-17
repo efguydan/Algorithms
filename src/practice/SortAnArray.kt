5ec099f582d6e37e6be4f581358dd3b8092df1d6
EfeDaniel <EfeEjemudaro@gmail.com>
2021-02-17 15:01:30 +0100

    Add SortAnArray solution

---
+++ src/practice/SortAnArray.kt
@@ -0,0 +1,41 @@
+package practice
+
+/**
+ * @author EfeDaniel
+ * Link: https://leetcode.com/problems/sort-an-array/
+ * Progress: Done
+ */
+object SortAnArray {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(sortArray(intArrayOf(5,2,3,1)).contentToString())
+    }
+
+    fun sortArray(nums: IntArray): IntArray {
+        val size = nums.size
+        for (i in (size / 2 - 1) downTo 0) heapify(nums, size, i)
+
+        for (i in size-1 downTo 0) {
+            nums[0] = nums[i].also { nums[i] = nums[0] }
+            heapify(nums, i, 0)
+        }
+
+        return nums;
+    }
+
+    private fun heapify(nums: IntArray, size: Int, i: Int) {
+        var largest = i
+        var left = 2 * i + 1
+        var right = 2 * i + 2
+
+        if (left < size && nums[left] > nums[largest]) largest = left
+        if (right < size && nums[right] > nums[largest]) largest = right
+
+        if (largest != i) {
+            nums[i] = nums[largest].also { nums[largest] = nums[i] }
+            heapify(nums, size, largest)
+        }
+    }
+
+}
