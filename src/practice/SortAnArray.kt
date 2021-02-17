c4abbc12d1bef18ddb1b31e1b50c818f96473fe2
EfeDaniel <EfeEjemudaro@gmail.com>
2021-02-17 15:07:42 +0100

    Add SortAnArray solution

--- src/practice/SortAnArray.kt
+++ src/practice/SortAnArray.kt
@@ -3,15 +3,27 @@ package practice
 /**
  * @author EfeDaniel
  * Link: https://leetcode.com/problems/sort-an-array/
+ * Time Complexity: O(n log n)
+ * Space Complexity: O(n log n)
  * Progress: Done
  */
 object SortAnArray {
 
+    /**
+     * Main method main used for testing purposes. Tests the input case:
+     * nums = [5, 2, 3, 1]
+     *
+     * output = [1, 2, 3, 5]
+     *
+     */
     @JvmStatic
     fun main(args: Array<String>) {
         println(sortArray(intArrayOf(5,2,3,1)).contentToString())
     }
 
+    /**
+     * Solution method. A sorting algorithm is implemented. Heap sort to be specific.
+     */
     fun sortArray(nums: IntArray): IntArray {
         val size = nums.size
         for (i in (size / 2 - 1) downTo 0) heapify(nums, size, i)
