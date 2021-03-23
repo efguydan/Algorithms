567bf08b6aaebdaf4df4190fead8e8b5866d89bf
EfeDaniel <EfeEjemudaro@gmail.com>
2021-03-23 22:17:57 +0100

    Chore: clean up code

--- src/practice/MiscKt.kt
+++ src/practice/MiscKt.kt
@@ -2,14 +2,10 @@ package practice
 
 
 fun main() {
-
+    println(1.0E22)
+    println(1.0E23)
+    println(1.0E24)
+    print(0.1 + 0.2)
 }
 
-fun solution(array1: Array<Int>, array2: Array<Int>, m: Int): Int {
-    return 0
-}
-
-fun dp(m: Int): Int {
-    return dp(0)
-}
 
