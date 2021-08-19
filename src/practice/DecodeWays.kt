1bec376790742de351e1c7cb9cf1c8b9d792305b
EfeDaniel <EfeEjemudaro@gmail.com>
2021-08-19 15:17:46 +0100

    Clean up Decode ways solution

--- src/practice/DecodeWays.kt
+++ src/practice/DecodeWays.kt
@@ -1,6 +1,7 @@
 package practice
 
 import java.lang.StringBuilder
+import kotlin.math.max
 
 /**
  * @author EfeDaniel
@@ -21,26 +22,13 @@ object DecodeWays {
             if (it.length == 2 && Character.getNumericValue(it[1]) == 0) return@map 1
             it.length
         }
-        var max = 0
-        for (length in combs) max = max.coerceAtLeast(length)
 
-        var fib = generateFibonacci(max)
+        val fib = combs.getMaxValue().coerceAtLeast(2).generateFibonacci()
         combs = combs.map { fib[it] }
 
         for (i in combs) answer = answer.coerceAtLeast(1) * i
 
         return answer
-    }
-
-    private fun generateFibonacci(n: Int): Array<Int> {
-        var fib = Array((n+1).coerceAtLeast(2)) {0}
-
-        fib[0] = 1
-        fib[1] = 1
-
-        for (i in 2..fib.lastIndex) fib[i] = fib[i-1] + fib[i-2]
-
-        return fib
     }
 
     private fun breakInputIntoPossibleCombinations(s: String): List<String> {
@@ -66,12 +54,12 @@ object DecodeWays {
                 sb.append(i)
                 possibleList.add(sb.toString())
                 sb.clear()
-            } else if (i > 2) {
-                sb.append(i)
-                possibleList.add(sb.toString())
-                sb.clear()
             } else {
                 sb.append(i)
+                if (i > 2) {
+                    possibleList.add(sb.toString())
+                    sb.clear()
+                }
             }
         }
         if (sb.isNotEmpty()) {
@@ -80,6 +68,18 @@ object DecodeWays {
         }
 
         return possibleList
+    }
+
+    private fun List<Int>.getMaxValue(): Int {
+        var max = 0
+        for (num in this) max = max(max, num)
+        return max
+    }
+
+    private fun Int.generateFibonacci() : Array<Int> {
+        val fib = Array((this+1)) { 1 }
+        for (i in 2..fib.lastIndex) fib[i] = fib[i-1] + fib[i-2]
+        return fib
     }
 
 }
