a11a32e66dbf0f2dd89f376386773bcab931f9c7
EfeDaniel <EfeEjemudaro@gmail.com>
2021-11-19 22:39:55 +0100

    clean up code

--- src/practice/MiscClass.java
+++ src/practice/MiscClass.java
@@ -1,11 +1,32 @@
 package practice;
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class MiscClass {
 
     public static void main(String[] args) {
+    }
 
+    public static int stockPairs(List<Integer> stocksProfit, long target) {
+        Set<Integer> keys = new HashSet<>();
+        Set<String> answer = new HashSet<>();
+
+        for (int num: stocksProfit) {
+            int counterpart = (int) (target - num);
+            if (keys.contains(counterpart)) {
+                answer.add(getKey(num, counterpart));
+            } else {
+                keys.add(num);
+            }
+        }
+
+        return answer.size();
+    }
+
+    private static String getKey(int a, int b) {
+        return a + "-" + b;
     }
 
 }
--- src/practice/MiscKt.kt
+++ src/practice/MiscKt.kt
@@ -6,30 +6,7 @@ object MiscKt {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println(getPair(listOf("abc", "bca", "dbe")).contentToString())
-        println(getPair(listOf("zzzz", "ferz", "zdsr", "fgtd")).contentToString())
-    }
 
-    fun getPair(strings: List<String>): IntArray {
-        val map = mutableMapOf<String, Int>()
-        for (i in strings.indices) {
-            val arr = strings[i].toCharArray()
-            for (j in arr.indices) {
-                val key = getKey(arr[j], j)
-                if (map.containsKey(key)) {
-                    return intArrayOf(map[key] ?: 0, i, j)
-                }
-                map[key] = i
-            }
-        }
-        return intArrayOf()
-    }
-
-    private fun getKey(char: Char, position: Int): String {
-        return buildString {
-            append(char)
-            append(position)
-        }
     }
 
 }
