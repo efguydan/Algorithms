f1b315603d9aa8192cb969ae056c062ee7c426cd
efedaniel <efeejemudaro@gmail.com>
2023-01-26 13:58:58 +0100

    Add SubdomainVisitCount Solution

---
+++ src/practice/SubdomainVisitCount.kt
@@ -0,0 +1,38 @@
+package practice
+
+/**
+ * @author EfeDaniel
+ * Link: https://leetcode.com/problems/subdomain-visit-count
+ * Progress: Done
+ */
+object SubdomainVisitCount {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(subdomainVisits(arrayOf("9001 discuss.leetcode.com")))
+        println(subdomainVisits(arrayOf("900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org")))
+    }
+
+    fun subdomainVisits(cpdomains: Array<String>): List<String> {
+        val map = mutableMapOf<String, Int>()
+        val answer = mutableListOf<String>()
+        for (domain in cpdomains) {
+            val split = domain.split(" ")
+            val count = split[0].toInt()
+            var url = split[1]
+            while (url.isNotEmpty()) {
+                map[url] = (map[url] ?: 0) + count
+                val firstIndex = url.indexOf('.')
+                if (firstIndex != -1) {
+                    url = url.removeRange(0, firstIndex+1)
+                } else {
+                    url = ""
+                }
+            }
+        }
+        for (key in map.keys) {
+            answer.add("${map[key]} $key")
+        }
+        return answer
+    }
+}
